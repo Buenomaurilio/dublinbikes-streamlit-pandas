bf4f35a69f143e99c45cd9ae694ad0f5b128a790
Buenomaurilio <mauriliodsb@gmail.com>
2025-06-14 13:04:34 +0100

    clean code

--- transformar_gold.py
+++ transformar_gold.py
@@ -1,31 +1,3 @@
-# import pandas as pd
-# from sqlalchemy import create_engine
-# from datetime import datetime
-
-# # Conexão com o PostgreSQL
-# engine = create_engine('postgresql://maurilio:147852@localhost:5432/dublin_bikes')
-
-# # Leitura da camada silver
-# df = pd.read_sql_table('dados_silver', con=engine)
-
-# # Tratamento: criar campo de ocupação
-# df['ocupacao'] = df['available_bikes'] / df['bike_stands']
-
-# # Agregação por estação (number + name)
-# df_gold = df.groupby(['number', 'name']).agg(
-#     total_bikes=pd.NamedAgg(column='available_bikes', aggfunc='sum'),
-#     media_bikes=pd.NamedAgg(column='available_bikes', aggfunc='mean'),
-#     ocupacao=pd.NamedAgg(column='ocupacao', aggfunc='mean'),
-#     qtd_registros=pd.NamedAgg(column='available_bikes', aggfunc='count'),
-#     data_hora=pd.NamedAgg(column='last_update', aggfunc='max')
-# ).reset_index()
-
-# # Salva na tabela 'dados_gold'
-# df_gold.to_sql('dados_gold', con=engine, if_exists='replace', index=False)
-
-# print("Dados salvos na camada gold.")
-
-
 import pandas as pd
 from sqlalchemy import create_engine
 
